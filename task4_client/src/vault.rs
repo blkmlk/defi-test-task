2c259a816c8cd17a5707fe4482486a160730d148
Islam Bekbuzarov <n157318@gmail.com>
2025-05-08 12:22:44 +0300

    version #2

---
+++ task4_client/src/vault.rs
@@ -0,0 +1,66 @@
+use sha2::Digest;
+use sha2::Sha256;
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    signature::Signer,
+    system_program,
+};
+
+fn anchor_discriminator(name: &str) -> [u8; 8] {
+    let mut hasher = Sha256::new();
+    hasher.update(format!("global:{}", name));
+    let hash = hasher.finalize();
+    let mut output = [0u8; 8];
+    output.copy_from_slice(&hash[..8]);
+    output
+}
+
+pub fn encode_initialize_ix(program_id: Pubkey, vault: Pubkey, user: Pubkey) -> Instruction {
+    let mut data = vec![];
+    data.extend_from_slice(&anchor_discriminator("initialize"));
+
+    Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(vault, false),
+            AccountMeta::new(user, true),
+            AccountMeta::new_readonly(system_program::id(), false),
+        ],
+        data,
+    }
+}
+
+pub fn encode_deposit_ix(
+    program_id: Pubkey,
+    vault: Pubkey,
+    user: Pubkey,
+    amount: u64,
+) -> Instruction {
+    let mut data = vec![];
+    data.extend_from_slice(&anchor_discriminator("deposit"));
+    data.extend_from_slice(&amount.to_le_bytes());
+
+    Instruction {
+        program_id,
+        accounts: vec![AccountMeta::new(vault, false), AccountMeta::new(user, true)],
+        data,
+    }
+}
+
+pub fn encode_withdraw_ix(
+    program_id: Pubkey,
+    vault: Pubkey,
+    user: Pubkey,
+    amount: u64,
+) -> Instruction {
+    let mut data = vec![];
+    data.extend_from_slice(&anchor_discriminator("withdraw"));
+    data.extend_from_slice(&amount.to_le_bytes());
+
+    Instruction {
+        program_id,
+        accounts: vec![AccountMeta::new(vault, false), AccountMeta::new(user, true)],
+        data,
+    }
+}
